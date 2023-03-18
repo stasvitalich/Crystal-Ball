ceba8e8750571f3c713844c19139ed81adf35976
stasvitalich <stasvitalich1994@gmail.com>
2023-03-18 23:39:57 +0600

    Predictions were aaded. Simple UI was created.

--- app/src/main/java/com/example/crystallball/MainActivity.kt
+++ app/src/main/java/com/example/crystallball/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.crystallball
-
+import render.animations.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.crystallball.databinding.ActivityMainBinding
@@ -7,10 +7,34 @@ import com.example.crystallball.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var prediction: String? = null
+    var random = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        pushButton()
     }
+
+    private fun getRandomNumber() {
+        random = (0 until Constants.predictions.size).random()
+    }
+
+    private fun getPrediction() {
+        prediction = Constants.predictions[random]
+    }
+
+    private fun pushButton() = binding.apply {
+
+        buttonPrediction.setOnClickListener {
+            getRandomNumber()
+            getPrediction()
+            textPrediction.text = prediction
+        }
+    }
+
+
 }
